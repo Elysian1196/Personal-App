8c42fcb3277206cebd93c606f6f74b0fb7a325e8
Elysian1196 <mdowns2015@brandeis.edu>
2017-06-14 11:58:06 -0400

    First commit

---
+++ pages/profile/client/profile.js
@@ -0,0 +1,11 @@
+const team =
+  [
+    {name:"Matthew Downs", email: "mdowns2015@brandeis.edu" },
+    {name:"Tim Hickey" , email: "tjhickey@brandeis.edu" }
+  ];
+
+Template.profile.helpers(
+ {
+   team
+ }
+)
