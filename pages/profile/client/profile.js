33b92b0d916e2a2faa94e040a64c66f2566ae03d
Elysian1196 <mdowns2015@brandeis.edu>
2017-06-26 00:20:15 -0400

    update to add meteor methods

--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -1,27 +1,27 @@
 Template.profile.events({
   'click #new' : function (e, instance) {
-    console.log('adding default')
-    current_id = Profiles.insert({name:'Default', picture: 'images/Bedouin Pharah.jpg', likes: []});
+    var to_be_inserted = {name:'Default', picture: 'images/Bedouin Pharah.jpg', likes: [], owner: Meteor.userId()};
+    Meteor.call('profiles.insert', Meteor.userId(), to_be_inserted);
   },
   'click #name_add' : function (e, instance) {
     console.log('changing name')
     const name = instance.$('#name').val();
     instance.$('#name').val("");
-    Profiles.update({_id:current_id}, {name: name});
+    Meteor.call('profiles.name.update', Meteor.userId(), name)
   },
   'click #picture_add' : function (e, instance) {
     const picture = instance.$('#picture').val();
     instance.$('#picture').val("");
-    Profiles.update({_id:current_id}, {picture: picture});
+    Meteor.call('profiles.picture.update', Meteor.userId(), picture);
   },
   'click #likes_add' : function (e, instance) {
     const like = instance.$('#likes').val();
     instance.$('#likes').val("");
-    Profiles.update({_id:current_id}, {$addToSet:{likes: like}});
+    Meteor.call('profiles.likes.add', Meteor.userId(), like);
   },
   'click #likes_delete' : function (e, instance) {
     const like = instance.$('#likes').val();
     instance.$('#likes').val("");
-    Profiles.update({_id:current_id}, {$pull:{likes: like}});
+    Meteor.call('profiles.likes.delete', Meteor.userId(), like);
   }
 })
