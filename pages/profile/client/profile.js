8acfdfc5149218c678a2cc3e1862896c2e06766f
Elysian1196 <mdowns2015@brandeis.edu>
2017-06-26 01:10:42 -0400

    yay my profile stuff works!

--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -1,6 +1,9 @@
+Template.profile.helpers({
+  current_profile() {return Profiles.find({owner: Meteor.userId()})},
+})
 Template.profile.events({
   'click #new' : function (e, instance) {
-    var to_be_inserted = {name:'Default', picture: 'images/Bedouin Pharah.jpg', likes: [], owner: Meteor.userId()};
+    var to_be_inserted = {name:'Default', picture: 'images/default_profile.jpg', likes: [], owner: Meteor.userId()};
     Meteor.call('profiles.insert', Meteor.userId(), to_be_inserted);
   },
   'click #name_add' : function (e, instance) {
